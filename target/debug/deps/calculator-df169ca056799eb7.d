8fe18ad7bbc6fc8cc474bb9bc3b31086019a41fd
Rustatano <jakub-svoboda111@seznam.cz>
2023-09-02 10:20:15 +0200

    "idk"

---
+++ target/debug/deps/calculator-df169ca056799eb7.d
@@ -0,0 +1,5 @@
+C:\Users\jakub\Documents\Rust\calculator\target\debug\deps\calculator-df169ca056799eb7.rmeta: src\main.rs
+
+C:\Users\jakub\Documents\Rust\calculator\target\debug\deps\calculator-df169ca056799eb7.d: src\main.rs
+
+src\main.rs:
