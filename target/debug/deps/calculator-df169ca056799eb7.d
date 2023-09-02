76c63b20c38fbd4d338faf9071c8e246ee8fe2d1
Rustatano <jakub-svoboda111@seznam.cz>
2023-09-02 10:39:30 +0200

    cleaning 4

--- target/debug/deps/calculator-df169ca056799eb7.d
+++
@@ -1,5 +0,0 @@
-C:\Users\jakub\Documents\Rust\calculator\target\debug\deps\calculator-df169ca056799eb7.rmeta: src\main.rs
-
-C:\Users\jakub\Documents\Rust\calculator\target\debug\deps\calculator-df169ca056799eb7.d: src\main.rs
-
-src\main.rs:
