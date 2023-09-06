058434c6cd60c43c463949767f92dd42be954950
Rustatano <108299332+Rustatano@users.noreply.github.com>
2023-09-06 19:19:28 +0200

    Delete target/release/build directory

--- target/release/build/winapi-dbe08d1872faaa5d/build_script_build-dbe08d1872faaa5d.d
+++
@@ -1,5 +0,0 @@
-C:\Users\jakub\Documents\Rust\calculator\target\release\build\winapi-dbe08d1872faaa5d\build_script_build-dbe08d1872faaa5d.exe: C:\Users\jakub\.cargo\registry\src\index.crates.io-6f17d22bba15001f\winapi-0.3.9\build.rs
-
-C:\Users\jakub\Documents\Rust\calculator\target\release\build\winapi-dbe08d1872faaa5d\build_script_build-dbe08d1872faaa5d.d: C:\Users\jakub\.cargo\registry\src\index.crates.io-6f17d22bba15001f\winapi-0.3.9\build.rs
-
-C:\Users\jakub\.cargo\registry\src\index.crates.io-6f17d22bba15001f\winapi-0.3.9\build.rs:
